88954be99e3903299911324dc6945dce51d62f8e
Btojaka <bruthtoj@gmail.com>
2024-12-30 13:13:41 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var server_1 = require("./server");
+console.log("desde indexe.ts");
+console.log("desde 2.ts");
+(0, server_1.sumar)(); // 5
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {sumar} from './server';
+
+console.log("desde indexe.ts");
+console.log("desde 3.ts");
+
+sumar(); // 5
+
