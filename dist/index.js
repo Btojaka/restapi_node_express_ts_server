d784040f458651ca6dad3d552037aa2975fe06ea
Btojaka <bruthtoj@gmail.com>
2024-12-30 13:42:47 +0100

    configurations project

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var server_1 = require("./server");
 console.log("desde indexe.ts");
-console.log("desde 2.ts");
+console.log("desde 3.ts");
 (0, server_1.sumar)(); // 5
