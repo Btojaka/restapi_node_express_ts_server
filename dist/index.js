d9e8b2bec5514132f367f7b0df0b107c7a92c367
Btojaka <bruthtoj@gmail.com>
2025-01-10 13:21:24 +0100

    deleted dist and coverage folders

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-console.log("desde indexe.ts");
-console.log("desde 3.ts");
-(0, server_1.sumar)(); // 5
-//# sourceMappingURL=index.js.map
